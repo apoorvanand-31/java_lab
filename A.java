6debeed62919c209b69e270cd8847c7387c211dc
apoorvanand-31 <apoorvanand.ai23@bmsce.ac.in>
2025-05-19 21:26:43 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,17 @@
+/*
+                                LAB-10(A)
+Define one class A in package apack. In class A, four variables are defined of access
+modifiers default, protected, private and public.*/
+
+
+
+
+// File: A.java
+package apack;
+
+public class A {
+    int defaultVar = 1;             // default access
+    protected int protectedVar = 2; // protected access
+    private int privateVar = 3;     // private access
+    public int publicVar = 4;       // public access
+}
